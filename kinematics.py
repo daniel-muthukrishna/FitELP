a0427f21d61fd77d3cd77851d06e660600b921b0
dmuthukrishna <daniel.muthukrishna@gmail.com>
2016-12-20 16:14:53 -0300

    Added red ErrorFlux file. Moved function read_spectra outside of class.

--- kinematics.py
+++ kinematics.py
@@ -9,29 +9,31 @@ from specutils.io import read_fits
 
 SpOfLi = 300000. #km/s
 
+
+def read_spectra(filename):
+    """ Reads spectra from input FITS File
+    Stores the wavelength (in Angstroms) in a vector 'x'
+    and the fluxes scaled by 10**14 in a vector 'y'
+    x and y are an array of the wavelengths and fluxes of each of the orders"""
+    x = []
+    y = []
+    spectra = read_fits.read_fits_spectrum1d(filename)  # , dispersion_unit=u.angstrom, flux_unit=u.cgs.erg/u.angstrom/u.cm**2/u.s)
+    for spectrum in spectra:
+        x.append(spectrum.dispersion / u.angstrom)
+        y.append(spectrum.flux * 1e14)
+    x = np.array(x)
+    y = np.array(y)
+
+    return x, y
+
+
 class GalaxyRegion(object):
     def __init__(self, specFileBlue, specFileRed, specFileBlueError, specFileRedError):
         """ x is wavelength arrays, y is flux arrays """
-        self.xBlue, self.yBlue = self.read_spectra(specFileBlue)
-        self.xRed, self.yRed = self.read_spectra(specFileRed)
-        self.xBlueError, self.yBlueError = self.read_spectra(specFileBlueError)
-        self.xRedError, self.yRedError = self.read_spectra(specFileRedError)
-
-    def read_spectra(self, filename):
-        """ Reads spectra from input FITS File
-        Stores the wavelength (in Angstroms) in a vector 'x'
-        and the fluxes scaled by 10**14 in a vector 'y'
-        x and y are an array of the wavelengths and fluxes of each of the orders"""
-        x = []
-        y = []
-        spectra = read_fits.read_fits_spectrum1d(filename)  # , dispersion_unit=u.angstrom, flux_unit=u.cgs.erg/u.angstrom/u.cm**2/u.s)
-        for spectrum in spectra:
-            x.append(spectrum.dispersion / u.angstrom)
-            y.append(spectrum.flux * 1e14)
-        x = np.array(x)
-        y = np.array(y)
-
-        return x, y
+        self.xBlue, self.yBlue = read_spectra(specFileBlue)
+        self.xRed, self.yRed = read_spectra(specFileRed)
+        self.xBlueError, self.yBlueError = read_spectra(specFileBlueError)
+        self.xRedError, self.yRedError = read_spectra(specFileRedError)
 
     def plot_order(self, orderNum, filter='red', minIndex=0, maxIndex=-1, title=''):
         """Plots the wavelength vs flux for a particular order. orderNum starts from 0"""
@@ -211,7 +213,7 @@ class FittingProfile(object):
 
 
 if __name__ == '__main__':
-    ngc6845_7 = GalaxyRegion('NGC6845_7B.fc.fits', 'NGC6845_7R.fc.fits', 'NGC6845_7B.fc.fits', 'NGC6845_7R_ErrorFlux.fc.fits')
+    ngc6845_7 = GalaxyRegion('NGC6845_7B.fc.fits', 'NGC6845_7R.fc.fits', 'NGC6845_7B.fc.fits', 'NGC6845_7R_ErrorFlux.fc.fits')  # Flux Calibrated
     # ngc6845_7.plot_order(20, filter='red', maxIndex=-10, title="NGC6845_7_red Order 21")
 
     # SPECTRAL LINE INFO FOR [H_ALPHA, H_BETA, H_GAMMA, H_DELTA]
@@ -241,8 +243,8 @@ if __name__ == '__main__':
             modelVoigt = fittingProfile.voigt_model_profile(c=vCenter, s=vSigma, g=vGamma)
 
         # FIT MULTI-COMPONENT GAUSSIAN
-        numOfComponentsList = [2, 2, 2, 2] # Number of components used for each emission line
-        centerList = [6329.27891, 6320, 6190.34511] # information for each of the three components
+        numOfComponentsList = [2, 2, 2, 2]  # Number of components used for each emission line
+        centerList = [6329.27891, 6320, 6190.34511]  # information for each of the three components
         centerMinList = [-np.inf, -np.inf, -np.inf]
         centerMaxList = [np.inf, np.inf, np.inf]
         sigmaList = [17.1169513, 90, 44.5836051]

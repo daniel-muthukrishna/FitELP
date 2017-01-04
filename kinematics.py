3517f412c989dc7340c14107af05038116f25ac1
dmuthukrishna <daniel.muthukrishna@gmail.com>
2017-01-04 18:53:16 -0300

    Varying center and sigma

--- kinematics.py
+++ kinematics.py
@@ -50,8 +50,8 @@ class GalaxyRegion(object):
 
         plt.figure(title)
         plt.title(title)
-        #plt.plot(x[orderNum][minIndex:maxIndex], y[orderNum][minIndex:maxIndex], label='Spectrum')
-        plt.plot(y[orderNum][minIndex:maxIndex])
+        plt.plot(x[orderNum][minIndex:maxIndex], y[orderNum][minIndex:maxIndex], label='Spectrum')
+        #plt.plot(y[orderNum][minIndex:maxIndex])
         if yE is not None:
             pass #plt.plot(xE[orderNum][minIndex:maxIndex], yE[orderNum][minIndex:maxIndex], label='Spectrum Error')
         plt.legend()
@@ -145,19 +145,33 @@ class FittingProfile(object):
         pars is the lmfit Parameters for the fit, prefix is the label of the gaussian, c is the center, s is sigma,
         a is amplitude. Returns the Gaussian model"""
         if 'H-Alpha' in self.lineName:
-            vary = True
+            varyCenter = True
+            varySigma = True
+            varyAmp = True
+        elif 'H-Gamma' in self.lineName:
+            varyCenter = False
+            varySigma = False
+            varyAmp = True
         else:
-            vary = True
-            cMin = c - 5
-            cMax = c + 5
-            sMin = s - 50
-            sMax = s + 50
+            varyCenter = False
+            varySigma = True
+            varyAmp = True
+            # cMin = c - c*0.00001
+            # cMax = c + c*0.00001
+            # sMin = s - s*0.05
+            # sMax = s + s*0.05
+            # aMin = a - a*0.05
+            # aMax = a + a*0.05
+
+
+
+
 
         g = GaussianModel(prefix=prefix)
         pars.update(g.make_params())
-        pars[prefix+'center'].set(c, min=cMin, max=cMax, vary=vary)
-        pars[prefix + 'sigma'].set(s, min=sMin, max=sMax, vary=vary)
-        pars[prefix + 'amplitude'].set(a, min=aMin, max=aMax)
+        pars[prefix+'center'].set(c, min=cMin, max=cMax, vary=varyCenter)
+        pars[prefix + 'sigma'].set(s, min=sMin, max=sMax, vary=varySigma)
+        pars[prefix + 'amplitude'].set(a, min=aMin, max=aMax, vary=varyAmp)
 
         return g
 
@@ -201,35 +215,40 @@ class FittingProfile(object):
 
 
 if __name__ == '__main__':
+    if ('a' or 'b') in 'db':
+        print True
+    else:
+        print False
+    plt.show()
     galaxyRegion = GalaxyRegion('NGC6845_7B.fc.fits', 'NGC6845_7R.fc.fits', specFileBlueError='NGC6845_7B_ErrorFlux.fc.fits', specFileRedError='NGC6845_7R_ErrorFlux.fc.fits', scaleFlux=1e14)  # Flux Calibrated
-    # galaxyRegion = GalaxyRegion('NGC6845_7B_SPEC1.wc.fits', 'NGC6845_7R_SPEC1.wc.fits', specFileBlueError='NGC6845_7B_VAR4.wc.fits', specFileRedError='NGC6845_7R_VAR4.wc.fits', scaleFlux=1)  # Counts (ADUS) Calibrated
-    #galaxyRegion.plot_order(3, filt='red', maxIndex=-10, title="NGC6845_7_red Order 21")
+    #galaxyRegion = GalaxyRegion('NGC6845_7B_SPEC1.wc.fits', 'NGC6845_7R_SPEC1.wc.fits', specFileBlueError='NGC6845_7B_VAR4.wc.fits', specFileRedError='NGC6845_7R_VAR4.wc.fits', scaleFlux=1)  # Counts (ADUS) Calibrated
+    #galaxyRegion.plot_order(3, filt='blue', maxIndex=-10, title="NGC6845_7_red Order 21")
 
     # SPECTRAL LINE INFO FOR [H_ALPHA, H_BETA, H_GAMMA, H_DELTA]
-    lineNames = ['H-Alpha ', 'H-Beta ', 'H-Gamma ', 'H-Delta ', 'OIII-5007A ', 'OIII-4959A ']
-    colour = ['b', 'g', 'r', 'c', 'm', 'y', 'k']
-    order = [20, 35, 27, 22, 4, 3]
+    lineNames = ['H-Alpha ', 'H-Beta ', 'H-Gamma ', 'H-Delta ', 'NII-6548A ', 'NII-6584A ', 'SII-6717A ', 'SII-6731A ', 'OII-3717A ', 'OII-3729A ', 'OII-7919A ', 'OII-7330A ', 'OI-6300A ', 'OI-6364A ', 'SIII-6312A ', 'SIII-9069A ', 'SIII-9535A ', 'ArIII-7136A ', 'ArIII-7751A ', 'He1H8-3889A ', 'HeI-4471A ', 'HeI-5876A ', 'HeI-6678A ', 'HeI-7065A ', 'HeI-7281A ', 'OIII-5007A ', 'OIII-4959A ', 'OIII-4363A ', 'NeIII-3868A  ', 'NeIII-3970A ']
+    colour = ['b', 'g', 'r', 'c', 'm', 'y', 'k', '#D35400', '#58D68D', '#EC7063', '#5D6D7E', '#F8C471', '#7FB3D5']
+    order = [20, 35, 27, 22]#, 4, 3]
     filt = ['red', 'blue', 'blue', 'blue', 'red', 'red']
-    minI = [1180, 2150, 500, 1300, 1600, 2300]
+    minI = [1180, 2150, 700, 1300, 1600, 2300]
     maxI = [1650, 2800, 1200, 2000, 2100, 2800]
     restWavelength = [6562.82, 4861.33, 4340.47, 4101.74, 5007, 4959]
-    numOfComponentsList = [2, 2, 2, 2, 2, 2]  # Number of components used for each emission line
-    numOfProfiles = len(lineNames)
+    numOfProfiles = len(order)
+    numOfComponentsList = [3] * numOfProfiles  # Number of components used for each emission line
 
     # Information for each of the three components
-    centerList = [6329.27, 6326.9, 6300]
+    centerList = [6349.2, 6328.978, 6314.2879]
     centerMinList = [-np.inf, -np.inf, -np.inf]
     centerMaxList = [np.inf, np.inf, np.inf]
-    sigmaList = [28.9, 69.4, 44.6]
+    sigmaList = [19.2858, 61.11, 21.3885036]
     sigmaMinList = [-np.inf, -np.inf, -np.inf]
     sigmaMaxList = [np.inf, np.inf, np.inf]
-    amplitudeList = [[48.3, 10.5, 10], [19.7, 4.4, 5], [10, 8.36, 5], [5.6, 1.75, 5], [5.6, 1.75, 5], [5.6, 1.75, 5], [5.6, 1.75, 5], [5.6, 1.75, 5], [5.6, 1.75, 5], [5.6, 1.75, 5]]
+    amplitudeList = [[17.1354, 15.3248335, 25.9915929], [19.7, 4.4, 5], [3.184, 7.7036, 4.444], [5.6, 1.75, 5], [7.62317429, 11.913, 8.83], [17.1354, 15.3248335, 25.9915929], [5.6, 1.75, 5], [5.6, 1.75, 5], [5.6, 1.75, 5], [5.6, 1.75, 5], [5.6, 1.75, 5], [5.6, 1.75, 5], [5.6, 1.75, 5], [5.6, 1.75, 5]]
     amplitudeMinList = [[-np.inf, -np.inf, -np.inf]]*numOfProfiles
     amplitudeMaxList = [[np.inf, np.inf, np.inf]]*numOfProfiles
-    linSlope = 1.1e-5
+    linSlope = 1.3796e-5
     linSlopeMin = -np.inf
     linSlopeMax = np.inf
-    linInt = -0.067
+    linInt = -0.07987
     linIntMin = -np.inf
     linIntMax = np.inf
 

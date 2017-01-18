ab1a7a39cdacf0f8a95398481bd00ab51aa2542c
dmuthukrishna <daniel.muthukrishna@gmail.com>
2017-01-18 12:16:21 -0300

    Output regionInfo Table. Fixed distance

--- profile_info_NGC6845_Region26.py
+++ profile_info_NGC6845_Region26.py
@@ -60,4 +60,4 @@ class RegionParameters(object):
     plottingXRange = [5900, 6500]  # velocities
     sigmaInstrBlue = 5.1
     sigmaInstrRed = 5.7
-    distance = 2e28  # Distance to region in centimetres
+    distance = 2.8e26  # Distance to region in centimetres (same units as flux)

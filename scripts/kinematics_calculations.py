a74e6b962a39de87c6f12421b972cec3e23131aa
daniel-muthukrishna <daniel.muthukrishna@gmail.com>
2019-03-18 20:53:38 +0000

    Correct printed fluxes in measurements_X by rp.scaleFlux

--- scripts/kinematics_calculations.py
+++ scripts/kinematics_calculations.py
@@ -159,18 +159,18 @@ def save_measurements(measurementInfo, rp):
                     #     idx = fluxInfoIdx
                     emName, flux, fluxErr, restWave, continuum, eW = fluxInfo[idx]
                     try:
-                        flux = np.format_float_scientific(float(flux) * rp.scaleFlux, precision=2)
-                        fluxErr = np.format_float_scientific(float(fluxErr) * rp.scaleFlux, precision=2)
-                        continuum = np.format_float_scientific(float(continuum) * rp.scaleFlux, precision=2)
-                        eW = np.format_float_scientific(float(eW) * rp.scaleFlux, precision=2)
+                        flux = np.format_float_scientific(float(flux) / rp.scaleFlux, precision=2)
+                        fluxErr = np.format_float_scientific(float(fluxErr) / rp.scaleFlux, precision=2)
+                        continuum = np.format_float_scientific(float(continuum) / rp.scaleFlux, precision=2)
+                        eW = np.format_float_scientific(float(eW) / rp.scaleFlux, precision=2)
                     except AttributeError:
                         print("Cannot convert floats to scientific notation because you are using an old "
                               "version of Numpy. Please update numpy.")
                         from decimal import Decimal
-                        flux = '%.2E' % Decimal(str(float(flux) * rp.scaleFlux))
-                        fluxErr = '%.2E' % Decimal(str(float(fluxErr) * rp.scaleFlux))
-                        continuum = '%.2E' % Decimal(str(float(continuum) * rp.scaleFlux))
-                        eW = '%.2E' % Decimal(str(float(eW) * rp.scaleFlux))
+                        flux = '%.2E' % Decimal(str(float(flux) / rp.scaleFlux))
+                        fluxErr = '%.2E' % Decimal(str(float(fluxErr) / rp.scaleFlux))
+                        continuum = '%.2E' % Decimal(str(float(continuum) / rp.scaleFlux))
+                        eW = '%.2E' % Decimal(str(float(eW) / rp.scaleFlux))
                     ionName, lambdaZero = line_label(emName, float(restWave))
                     ionName = ionName.strip('$').replace("\\", "").replace('mathrm{', "").replace('}', '').split('_')[0]
                     lambdaZero = lambdaZero.strip('$')

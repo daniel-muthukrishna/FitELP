48f03bfe4a5347592eff13de3d8c8b67e488d70c
daniel-muthukrishna <daniel.muthukrishna@gmail.com>
2019-07-16 14:31:19 -0700

    Added autoapi documentation options

--- docs/conf.py
+++ docs/conf.py
@@ -47,6 +47,8 @@ extensions = [
     'autoapi.extension'
 ]
 
+autoapi_dirs = ['../src']
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
